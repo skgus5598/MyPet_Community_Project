6836dac2dab11613a46336665a271b2de5862720
raina <skgus5598@gmail.com>
2022-07-04 22:26:46 +0900

    New project

---
+++ src/test/java/com/rainaq/mypet/MypetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainaq.mypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
